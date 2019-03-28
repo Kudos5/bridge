96e420a05894b4b6ffc163c2ce0336a13a006f6a
Anders <anderssp1994@gmail.com>
2019-03-28 18:11:29 +0100

    Fix order of using directives

--- test.cpp
+++ test.cpp
@@ -2,8 +2,8 @@
 #include "bridge.hpp"
 
 int main() {
+	using Bridge::Bridge;
     using namespace Bridge;
-    using Bridge::Bridge;
     Bridge b;
     Card c{Suit::Spades, Rank::Ace};
     b.Play(c);
