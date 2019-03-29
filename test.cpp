39de8ed9b7ef83a05b5365f2872d54c991030dc2
Anders <anderssp1994@gmail.com>
2019-03-29 21:25:48 +0100

    Add testing

--- test.cpp
+++ test.cpp
@@ -1,10 +1,15 @@
 
+#define CATCH_CONFIG_MAIN
+#include "catch2/catch.hpp"
+
 #include "bridge.hpp"
 
-int main() {
+TEST_CASE( "Card is played", "[Bridge::Play]" ) {
 	using Bridge::Bridge;
     using namespace Bridge;
     Bridge b;
     Card c{Suit::Spades, Rank::Ace};
     b.Play(c);
+    REQUIRE( true );
 }
+
