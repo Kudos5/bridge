1297a3ee41aa1640c81de91390843ae230dedf99
Anders <anderssp1994@gmail.com>
2019-03-29 23:22:27 +0100

    Implement compile-time test

--- test.cpp
+++ test.cpp
@@ -1,19 +1,28 @@
 
 #define CATCH_CONFIG_MAIN
+#define GSL_THROW_ON_CONTRACT_VIOLATION
 #include "catch2/catch.hpp"
 
 #include "bridge.hpp"
+
+// Taken from https://github.com/lefticus/cpp_box/blob/master/test/constexpr_tests.cpp
+template<bool B> bool static_test()
+{
+  static_assert(B);
+  return B;
+}
+#define REQUIRE_STATIC(X) REQUIRE( static_test<(X)>() )
 
 SCENARIO( "cards can be played ", "[bridge]" ) {
 	using Bridge::Bridge;
     using namespace Bridge;
     GIVEN( "A game of bridge" ) {
-        Bridge b;
+        constexpr Bridge b(Player::West, Vulnerability::None);
         WHEN( "a card is played" ) {
-            Card c{Suit::Spades, Rank::Ace};
+            constexpr Card c{Suit::Spades, Rank::Ace};
             b.Play(c);
             THEN( "nothing happens" ) {
-                REQUIRE( false );
+                REQUIRE_STATIC( true );
             }
         }
     }
